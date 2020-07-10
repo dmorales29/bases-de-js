4d43f1117da36a491cf3a18486f94f130ee2da3d
David Morales <morald01@stepstone.com>
2020-07-09 23:17:29 -0600

    Tipos de datos en JS y variables

---
+++ js/valores.js
@@ -0,0 +1,26 @@
+typeof 40;
+// "number"
+
+typeof David;
+// "undefined"
+
+typeof "David";
+// "string"
+
+typeof true;
+// "boolean"
+
+typeof false;
+// "boolean"
+
+typeof null;
+// "object"
+
+typeof undefined;
+// "undefined"
+
+typeof [1, 2];
+// "object"
+
+typeof { name: "David" };
+// "object"
---
+++ js/variables.js
@@ -0,0 +1,16 @@
+var nombre = "David";
+
+// Declaración
+var edad;
+
+//Inicializar la varible
+edad = 30;
+
+//Variable array
+var elementos = ["Computadora", "Celular"];
+
+//Variable objeto
+var persona = {
+  nombre: "David",
+  edad: 30,
+};
