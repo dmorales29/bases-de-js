d5fc8817f6ad8f558cd2c0293ea1aa611bbe981b
David Morales <morald01@stepstone.com>
2020-07-09 23:47:27 -0600

    Funciones

---
+++ js/funciones.js
@@ -0,0 +1,22 @@
+// Función declarativas
+function myFunction() {
+  return 3;
+}
+
+// Función de expresión o anónimas
+var anotherFunction = function (a, b) {
+  return a + b;
+};
+
+//Llamada de la función
+myFunction();
+anotherFunction();
+
+//Ejemplo
+function saludarEstudiantes(estudiante) {
+  console.log(`Hola ${estudiante}`);
+}
+
+function sumar(a, b) {
+  return a + b;
+}
