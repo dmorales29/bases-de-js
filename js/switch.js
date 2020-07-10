20c89c2d582b1e2a2721a59fb16c988624c4b136
David Morales <morald01@stepstone.com>
2020-07-10 14:18:23 -0600

    Switch

---
+++ js/switch.js
@@ -0,0 +1,16 @@
+//Es similar a las validadciones de If else, pero funciona por "casos"
+
+var numero = "a";
+
+switch (numero) {
+  case 1:
+    console.log("Es uno");
+    break;
+
+  case 10:
+    console.log("Es un 10");
+    break;
+
+  default:
+    console.log("No es un número");
+}
